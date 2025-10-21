6407e959f0e91d4c4082a519a5b5015cda612668
alexgiovannir <alexgiovannir@gmail.com>
2025-10-21 20:41:35 -0300

    initial commit

--- Terraform/module/aws_ecs/ec2-launch-template.tf
+++ Terraform/module/aws_ecs/ec2-launch-template.tf
@@ -2,7 +2,6 @@ resource "aws_launch_template" "ec2_lt" {
   name_prefix = "ecs-template"
   image_id = "ami-0c45946ade6066f3d"
   instance_type = "t2.micro"
-  key_name = "humangov-ec2-key"
   vpc_security_group_ids = [var.lanchonete_security_group_id]
   iam_instance_profile {
     name = "ecs-adm-role"
